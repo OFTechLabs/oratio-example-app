56721f4459a28d8c97087d41de562a11c97803bd
jelleverstegen <jelle.verstegen@gmail.com>
2017-08-18 14:42:36 +0200

    add easter egg neuron for fun and to show how handling input to match dynamic output could work

---
+++ src/app/oratio/general/EasterEggs.ts
@@ -0,0 +1,36 @@
+
+export interface EasterEgg {
+  input: string;
+  output: string;
+}
+
+export const easterEggs: EasterEgg[]  = [
+  {
+    input: 'use the force',
+    output: 'I\'m not sure, my powerlevel is over 9000!'
+  },
+  {
+    input: 'I am your father',
+    output: 'My father is my mother, so who are you exactly?'
+  },
+  {
+    input: 'who you gonna call',
+    output: 'Your girlfriend... JK, calling Ghostbusters'
+  },
+  {
+    input: 'are you skynet',
+    output: 'Yes, it begins in 3... 2.... 1...'
+  },
+  {
+    input: 'winter is coming',
+    output: 'for house Frey, king in the north!'
+  },
+  {
+    input: 'the matrix',
+    output: '*whispers* Don\'t speak of such things.'
+  },
+  {
+    input: 'do you feel lucky',
+    output: 'I do, not lucky enough to challenge Clint, but still.'
+  },
+]
